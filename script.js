64a7413c985c8a38334e0c510d6ad7c32a6ebd45
Dexter Vorbe <dextervorbe@ymail.com>
2022-05-26 22:38:28 -0400

    Added Score Functionality
    Created Score Functionality for Guessing game

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 'use strict';
 
-const number = Math.trunc(Math.random() *20) + 1;
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').value = number;
+let score = 20;
 
 console.log(number);
 
-
 document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value)
-    console.log(guess);
+  const guess = Number(document.querySelector('.guess').value);
 
-    if(!guess) {
-        document.querySelector('.message').textContent = '👀 ummmm..... I do not see any numbers inputed, try again';
-    } 
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      '👀 ummmm..... I do not see any numbers inputed, try again';
+  } else if (guess === number) {
+    document.querySelector('.message').textContent =
+      '👏 You are a guessing wizard 🙌';
+  } else if (guess > number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🥵 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        '😇 You luck has run out';
+        document.querySelector('.score').textContent = 0;
+        
+    }
+  } else if (guess < number) {
+      if(score > 1) {
+          document.querySelector('.message').textContent = '🥶 Too Low';
+          score--;
+          document.querySelector('.score').textContent = score;
+
+      } else {
+        document.querySelector('.message').textContent =
+        '😇 You luck has run out';
+        document.querySelector('.score').textContent = 0;
+      }
+  } 
 });
-
-
-
-
-
-
-
-
-
-
-
-

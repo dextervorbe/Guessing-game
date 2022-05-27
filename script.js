bdfe395e128d13e94ec38bdb9f52af4b1bbe0c25
Dexter Vorbe <dextervorbe@ymail.com>
2022-05-26 21:39:42 -0400

    Added Click Event
    Added click event. On click will fire function to add user value to guess
    
    If no value code will notify player to input a value
    
    also added random number

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 'use strict';
-document.querySelector('.message').textContent;
+
+const number = Math.trunc(Math.random() *20) + 1;
+
+console.log(number);
+
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess);
+
+    if(!guess) {
+        document.querySelector('.message').textContent = '👀 ummmm..... I do not see any numbers inputed, try again';
+    } 
+});
+
+
+
+
+
+
+
+
+
+
+
+

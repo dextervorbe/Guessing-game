c89d13b3005b125be512d96e6bc3fff22950181d
Dexter Vorbe <dextervorbe@ymail.com>
2022-05-26 22:58:32 -0400

    Again Button Functionality
    Made it so the page reloads when you press the AGAIN button

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ let score = 20;
 
 console.log(number);
 
+
+document.querySelector('.again').addEventListener('click', function() {
+
+window.location.reload()
+
+alert('Press Ok to start');
+
+})
+
+
+
+
+
+
+
+
+
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -15,6 +34,10 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === number) {
     document.querySelector('.message').textContent =
       '👏 You are a guessing wizard 🙌';
+
+    document.querySelector('.number').textContent = number;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
   } else if (guess > number) {
     if (score > 1) {
       document.querySelector('.message').textContent = '🥵 Too High';
@@ -23,19 +46,17 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent =
         '😇 You luck has run out';
-        document.querySelector('.score').textContent = 0;
-        
+      document.querySelector('.score').textContent = 0;
     }
   } else if (guess < number) {
-      if(score > 1) {
-          document.querySelector('.message').textContent = '🥶 Too Low';
-          score--;
-          document.querySelector('.score').textContent = score;
-
-      } else {
-        document.querySelector('.message').textContent =
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🥶 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
         '😇 You luck has run out';
-        document.querySelector('.score').textContent = 0;
-      }
-  } 
+      document.querySelector('.score').textContent = 0;
+    }
+  }
 });
